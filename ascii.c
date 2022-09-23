6a9017721a1f1351002b689898e000b3eb64d06a
Pibil Francis <billfranc20@gmail.com>
2022-09-23 18:42:54 +0000

    add

---
+++ ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* This program prints the ASCII you want*/
+
+int main()
+{
+    char ch; //varuable declearation
+    printf("Enter a character");
+    scanf("%c",&ch);  // user input
+    printf("\n The ascii value of the ch variable is : %d", ch);
+    return 0;
+}
